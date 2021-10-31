a700fbf66a90e1b9a4ee2c96ec50a9d855c4a58f
Luigi Foscari <luigi.foscari@icloud.com>
2021-10-31 17:26:00 +0100

    Second problem

--- main.ml
+++ main.ml
@@ -6,3 +6,8 @@ let rec last = function
     | _ :: xs -> last xs
     | [] -> None
 
+(* 2 *)
+let rec last_two = function
+    | x :: y :: [] -> Some (x, y)
+    | _ :: xs -> last_two xs
+    | [] -> None
