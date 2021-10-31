94df6d6a437e65f4d1cc30bc193cc271313380d3
Luigi Foscari <luigi.foscari@icloud.com>
2021-10-31 17:28:41 +0100

    Third problem

--- main.ml
+++ main.ml
@@ -11,3 +11,10 @@ let rec last_two = function
     | x :: y :: [] -> Some (x, y)
     | _ :: xs -> last_two xs
     | [] -> None
+
+(* 3 *)
+let rec at index ls =
+    match ls with
+    | x :: _  when index == 1 -> Some x
+    | _ :: xs when index > 1 -> at ( index - 1 ) xs
+    | _ -> None
